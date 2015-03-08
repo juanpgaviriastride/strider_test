e49f547c86cabe494ecce011ec77e3614a1bb06f
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2015-03-07 19:36:14 -0500

    Fix null pointer exception with titanium connection, still not writing to cassandra

--- com.webtalk.storage/src/com/webtalk/storage/graph/config.clj
+++ com.webtalk.storage/src/com/webtalk/storage/graph/config.clj
@@ -5,19 +5,31 @@
 
 (def graph-config
   {
-   "storage.backend"  "cassandra"
+   "storage.backend" "cassandra"
    "storage.hostname" "192.168.50.4"
-   "storage.keyspace" "titan"
-   "graph" "graph"
+   ;; "storage.keyspace" "titan"
+   ;; "graph-name" "graph"
+
+   ;; "storage.index.search.backend" "elasticsearch"
+   ;; "storage.index.search.hostname" "192.168.50.4"
+   ;; "storage.index.search.client-only" "true"
+   ;; "storage.index.search.cluster-name" "webtalk"
+   ;; "storage.index.search.local-mode" "false"
+   ;; "storage.index.search.sniff" "false"
+   ;; "storage.backend"  "cassandra"
+   ;; "storage.hostname" "192.168.50.4"
+   ;; "storage.keyspace" "titan"
+   ;; "graph" "graph"
    })
 
 (defn -main
   [& args]
   (println "Connecting" (str graph-config))
-  (let [g (tgraph/open graph-config)]
-    (tgraph/new-transaction
+  (let [graph (tgraph/open graph-config)]
+    (tgraph/with-transaction [g graph]
       (let [ghost1 (tvertex/create! g {:VertexType "ghost" :name "ghost 1"})
             ghost2 (tvertex/create! g {:VertexType "ghost" :name "ghost 2"})]
-        (println (map tvertex/to-map (tvertex/get-all-vertices g)))
-        (tgraph/commit g)))
-    (tgraph/shutdown g)))
+        (println ghost1 ghost2)
+        (println (map tvertex/to-map (tvertex/get-all-vertices g)))))
+    (println "Shutting down" (str graph))
+    (tgraph/shutdown graph)))
