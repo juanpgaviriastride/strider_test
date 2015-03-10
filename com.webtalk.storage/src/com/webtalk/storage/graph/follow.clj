e0def0e7742fec01db446cbef80d939b5486315b
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2015-03-10 14:13:38 -0500

    Add follow action to clojure storage code

--- com.webtalk.storage/src/com/webtalk/storage/graph/follow.clj
+++ com.webtalk.storage/src/com/webtalk/storage/graph/follow.clj
@@ -1,5 +1,16 @@
 (ns com.webtalk.storage.graph.follow
   (:gen-class)
-  [clojurewerkz.titanium.graph    :as tgraph]
+  (:require [clojurewerkz.titanium.graph    :as tgraph]
             [clojurewerkz.titanium.vertices :as tvertex]
-            [clojurewerkz.titanium.edges    :as tedge])
+            [clojurewerkz.titanium.edges    :as tedge]))
+
+(defn follow!
+  "It adds an edge between the user_id vertex and
+   followed_id vetex.
+
+  Example: (follow! graph {\"user_id\" ID \"followed_id\" ID})"
+  [graph payload]
+  (let [user     (tvertex/find-by-id graph (payload "user_id"))
+        followed (tvertex/find-by-id graph (payload "followed_id"))]
+    (tgraph/with-transaction [g graph]
+      (tedge/upconnect! g user "follow" followed))))
