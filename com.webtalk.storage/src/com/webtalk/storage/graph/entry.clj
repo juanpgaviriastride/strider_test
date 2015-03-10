61d659e72e8dd2f745688478b2c8f10cf59567fc
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2015-03-10 11:43:38 -0500

    Add entry creation logic

--- com.webtalk.storage/src/com/webtalk/storage/graph/entry.clj
+++ com.webtalk.storage/src/com/webtalk/storage/graph/entry.clj
@@ -1,2 +1,22 @@
 (ns com.webtalk.storage.graph.entry
-  (:gen-class))
+  (:gen-class)
+  (:require [clojurewerkz.titanium.graph    :as tgraph]
+            [clojurewerkz.titanium.vertices :as tvertex]
+            [clojurewerkz.titanium.edges    :as tedge]))
+
+(defn entry-hash [payload]
+  (into {} (list payload
+                 {:VertexType "entry"})))
+
+(defn create-entry!
+  "Creates an entry vertex and links as follow:
+   owner -> posted -> entry
+   
+   Example: (create-entry! graph {\"user_id\" ID \"content\" \"Hi I'm here and I'm happy\" \"title\" \"Hello WT\"})"
+
+  [graph payload]
+  (let [owner (tvertex/find-by-id graph (payload "user_id"))]
+    (tgraph/with-transaction [g graph]
+      (let [entry (tvertex/create! g (entry-hash payload))]
+        (tedge/upconnect! g owner "posted" entry {:time (System/currentTimeMillis)})
+        entry))))
