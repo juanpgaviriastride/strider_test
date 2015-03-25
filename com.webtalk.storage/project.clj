6b1dc91d860e0ab573d1f0c130e0c6f10758c706
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2015-03-24 23:54:08 -0500

    Add hystrix streaming commands
    Pending:
    * Fix servlet import
    * Add turbine to aggregate

--- com.webtalk.storage/project.clj
+++ com.webtalk.storage/project.clj
@@ -4,19 +4,22 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [http-kit "2.1.18"]
                  [clojurewerkz/titanium "1.0.0-beta2"]
                  [clojurewerkz/cassaforte "2.0.0"]
                  [clojurewerkz/elastisch "2.2.0-beta1"]
-                 [clojurewerkz/route-one "1.1.0"]
-                 [clojurewerkz/mailer "1.2.0"]
-                 [clojurewerkz/serialism "1.3.0"]
+                 ;; [clojurewerkz/route-one "1.1.0"]
+                 ;; [clojurewerkz/mailer "1.2.0"]
+                 ;; [clojurewerkz/serialism "1.3.0"]
                  [org.clojure/data.json "0.2.5"]
-                 [com.taoensso/timbre "3.4.0"]
+                 ;; [com.taoensso/timbre "3.4.0"]
                  [com.novemberain/langohr "3.1.0"]
-                 [com.novemberain/validateur "2.4.2"]
+                 ;; [com.novemberain/validateur "2.4.2"]
                  [com.netflix.hystrix/hystrix-clj "1.4.1"]
+                 [hystrix-event-stream-clj "0.1.3"]
+                 [compojure "1.3.2"]
+                 [ring "1.3.2"]
                  [com.thinkaurelius.titan/titan-all "0.5.0"]]
   :main ^:skip-aot com.webtalk.storage
+  :aliases { "start-hystrix-server" ["run" "-m" "com.webtalk.resilience"]}
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
---
+++ com.webtalk.storage/src/com/webtalk/resilience.clj
@@ -0,0 +1,39 @@
+(ns com.webtalk.resilience
+  (:import (com.netflix.hystrix.contrib.metrics.eventstream HystrixMetricsStreamServlet)
+           (org.eclipse.jetty.server Server Request)
+           (org.eclipse.jetty.server.handler AbstractHandler)
+           (org.eclipse.jetty.server.nio SelectChannelConnector)
+           (org.eclipse.jetty.server.ssl SslSelectChannelConnector)
+           (org.eclipse.jetty.server.bio SocketConnector)
+           (org.eclipse.jetty.server.ssl SslSocketConnector)
+           (org.eclipse.jetty.servlet ServletContextHandler ServletHolder)
+           (org.eclipse.jetty.util.thread QueuedThreadPool)
+           (org.eclipse.jetty.util.ssl SslContextFactory)
+           (javax.servlet.http HttpServletRequest HttpServletResponse))
+  (:require
+   [compojure.core     :refer :all]
+   [ring.adapter.jetty :as jetty]
+   [ring.util.servlet  :as servlet]))
+
+(defn run-jetty-with-hystrix-stream [app options]
+  (let [^Server server (#'jetty/create-server (dissoc options :configurator))
+        ^QueuedThreadPool pool (QueuedThreadPool. ^Integer (options :max-threads 50))]
+    (when (:daemon? options false) (.setDaemon pool true))
+    (doto server (.setThreadPool pool))
+    (when-let [configurator (:configurator options)]
+      (configurator server))
+    (let [hystrix-holder  (ServletHolder. HystrixMetricsStreamServlet)
+          app-holder (ServletHolder. (servlet/servlet app))
+          context (ServletContextHandler. server "/" ServletContextHandler/SESSIONS)]
+      (.addServlet context hystrix-holder "/hystrix.stream")
+      (.addServlet context app-holder "/"))
+    (.start server)
+    (when (:join? options true) (.join server))
+    server))
+
+(defroutes app (GET "/hello" [] {:status 200 :body "Hello"}))
+
+  
+(def -main
+  [port]
+  (run-jetty-with-hystrix app {:port port :join? false}))
