a9425098b0607e4aab1906abe0d3c9612e0f7e3f
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2015-04-28 02:03:17 -0500

    Improve sendgrid wrapper

--- com.webtalk.storage/src/com/webtalk/mailer.clj
+++ com.webtalk.storage/src/com/webtalk/mailer.clj
@@ -4,16 +4,13 @@
                          SendGrid$Email
                          SendGrid$Response)))
 
-(defn send-email [auth params]
-  (let [{username :api_user password :api_key} auth
-        to (params :to)
-        from (params :from)
-        subject (params :subject)
-        html (params :html)
-        sendgrid (SendGrid. username password)
+(defn send-email
+  [{username :api_user password :api_key} {to :to from :from subject :subject html :html}]
+  (let [sendgrid (SendGrid. username password)
         email (-> (SendGrid$Email.)
                   (.addTo to)
                   (.setFrom from)
                   (.setSubject subject)
-                  (.setHtml html))]
-    (println (.getMessage (.send sendgrid email)))))
+                  (.setHtml html))
+        response (.send sendgrid email)]
+    (.getMessage response)))
