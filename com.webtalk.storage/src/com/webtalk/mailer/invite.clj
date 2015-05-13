9cae795fff64fcfb76ecbd851eb69d0a9b5bb598
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2015-05-13 13:59:43 -0500

    Update invitation template

--- com.webtalk.storage/src/com/webtalk/mailer/invite.clj
+++ com.webtalk.storage/src/com/webtalk/mailer/invite.clj
@@ -9,11 +9,12 @@
   (mailer/send-email config/auth
                      {:to to
                       :from "team@webtalk.co"
-                      :subject "[Webtalk] You have been invited to join us"
+                      :subject (str "Webtalk | Congratulations! " "chila" " has recommended you!")
                       :html (template/render-resource
                              "templates/invite.html.mustache"
                              {:name "Chila"
                               :position "NullDev at Nullindustries"
                               :image_url "https://scontent-mia.xx.fbcdn.net/hphotos-xfp1/t31.0-8/s720x720/132514_158085784241321_7607493_o.jpg"
-                              :join_url "http://localhost:3000/"
-                              :sign_in_url "http://localhost:3000/"})}))
+                              :join_url "http://localhost:3000/join"
+                              :sign_in_url "http://localhost:3000/signin"
+                              :profile_url "http://localhost:3000/chila"})}))
