78bb352070738670438835f8e363cd5079f9e67c
Hernan Metaute <hernan.metaute@gmail.com>
2015-10-30 18:14:46 -0500

    added persistence layer in order to model each resource attached to a persistence system

---
+++ src/wt/persistence/user.clj
@@ -0,0 +1,16 @@
+(ns wt.persistence.user
+  (:require
+   [wt.db.core :as db]
+   [bcrypt-clj.auth :refer :all])
+  (:import [java.sql.Date]))
+
+
+(defn save [user-map]
+  (db/create-user! {:name (:name user-map)
+                    :email (:email user-map)
+                    :birthday (db/sql-date (:birthday user-map))
+                    :gender (:gender user-map)
+                    :pass (crypt-password (:pass user-map))
+                    :admin false
+                    :last_login nil
+                    :is_active true}))
