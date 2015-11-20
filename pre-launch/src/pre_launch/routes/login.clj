11530214ba06069fc455014547720e0ff0b6dd95
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2015-11-20 13:12:04 -0500

    Add basic login routes

---
+++ pre-launch/src/pre_launch/routes/login.clj
@@ -0,0 +1,30 @@
+(ns pre-launch.routes.login
+  (:require [pre-launch.layout :as layout]
+            [compojure.core :refer :all]
+            [ring.util.http-response :refer [ok]]
+            [clojure.java.io :as io]
+            [ring.util.response :refer [response]]
+            ring.middleware.session))
+
+(defn check-password [email passw]
+  :ok)
+
+(defn login! [{{email :email passw :passw} :params
+               session :session}]
+  (if-let [identity (check-password email passw)]
+    (do
+      (assoc :session (assoc session :identity identity))
+      {:status 200
+       :headers {"Content-Type" "text/html"}
+       :body "rico"})
+    {:status 403
+     :headers {}
+     :body "Not authorized"}))
+
+(defn login [{session :session}]
+  (response "holz"))
+
+(defroutes login-routes
+  (GET "/signin" request (login request))
+  (POST "/login" request (login! request)))
+
