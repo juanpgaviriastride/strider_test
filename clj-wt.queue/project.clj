8779d9c58d613285d98cae8b1c26f42ddcc342ff
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2015-11-21 13:50:35 -0500

    Add environ dep for clj-wt queue

--- clj-wt.queue/project.clj
+++ clj-wt.queue/project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.5"]
+                 [environ "1.0.0"]
                  [com.novemberain/langohr "3.1.0"]])
