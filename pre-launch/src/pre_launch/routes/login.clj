e81cb00d4ecfda1f7f9c6648f91e37d72592efb8
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2015-11-23 12:52:31 -0500

    Create and link the signup pages

--- pre-launch/src/pre_launch/routes/login.clj
+++ pre-launch/src/pre_launch/routes/login.clj
@@ -24,7 +24,16 @@
 (defn login [{session :session}]
   (layout/render  "crowdfunding/login.html"))
 
+(defn signup [request]
+  (layout/render "crowdfunding/new-account.html"))
+
+(defn signup! [request]
+  "needs to actually create user this is a placeholder"
+  (layout/render "crowdfunding/new-account.html"))
+
 (defroutes login-routes
-  (GET "/signin" request (login request))
-  (POST "/login" request (login! request)))
+  (GET  "/signin" request (login request))
+  (POST "/login"  request (login! request))
+  (GET  "/signup" request (signup request))
+  (POST "/signup" request (signup! request)))
 
