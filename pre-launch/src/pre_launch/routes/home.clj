9f016442aa6c040eb6f7727dd3a6b683620bf049
Hernan Metaute <hernan.metaute@gmail.com>
2015-11-23 19:05:28 -0500

    added post to request an invite

--- pre-launch/src/pre_launch/routes/home.clj
+++ pre-launch/src/pre_launch/routes/home.clj
@@ -1,17 +1,25 @@
 (ns pre-launch.routes.home
   (:require [pre-launch.layout :as layout]
-            [compojure.core :refer [defroutes GET]]
+            [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :refer [ok]]
+            [ring.middleware.params]
+            [pre-launch.controllers.invitation-request :as controller]
             [clojure.java.io :as io]))
 
- (defn home-page []
+ (defn home-page [success]
      (layout/render
-      "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+      "home.html" {:docs (-> "docs/docs.md" io/resource slurp)
+                   :success success}))
 
-(defn about-page []
-  (layout/render "about.html"))
+(defn new-account-page []
+  (layout/render "crowdfunding/new-account.html"))
+
+(defn join-wait-list [email]
+  (controller/join-wait-list email)
+  (home-page true))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+  (GET "/" [] (home-page false))
+  (POST "/request-invitation" [email] (join-wait-list email))
+  (GET "/new-account" [] (new-account-page)))
 
