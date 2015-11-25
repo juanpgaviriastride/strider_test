e30c36187388535b544b4f70198e1f4a830d6940
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2015-11-25 16:52:19 -0500

    Use bean instead of Gson to handle java objects

--- pre-launch/src/pre_launch/integration/stripe.clj
+++ pre-launch/src/pre_launch/integration/stripe.clj
@@ -30,9 +30,36 @@
      (.setApiKey (private-key))
      .build))
 
-(defn create-customer! [customer-params]
-  (Customer/create customer-params (default-request-options)))
+(defn customer-hash [payload]
+  (into {} (list payload {:stripe_id (payload :id)
+                          :default_source (payload :defaultSource)
+                          :currency "usd"
+                          :description ""
+                          :connect_access_token ""
+                          :refresh_token ""
+                          :token_type ""
+                          :sources (json/write-str (bean (payload :sources)))
+                          :scope ""})))
 
+(defn customer-from-response [customer-object]
+  (-> customer-object
+     bean
+     (select-keys [:sources :id :email :defaultSource])
+     customer-hash))
+
+(defn create-customer! [customer-params]
+  (customer-from-response (Customer/create customer-params (default-request-options))))
+
+(defn charge-hash [payload]
+  (into {} (list payload {,,,
+                          ;;defaults here
+                          })))
+
+(defn charge-from-response [charge-object]
+  (-> charge-object
+     bean
+     (select-keys [:description :disputed :applicationFee :amount :failureMessage :transfer :captured :dispute :created :source :statementDescription :failureCode :customer :balanceTransaction :receiptEmail :card :invoice :currency :refunded :amountRefunded :status :id :class :statementDescriptor :paid :fraudDetails :livemode :shipping :receiptNumber :metadata :destination :refunds])
+     charge-hash))
 (defn create-charge! [charge-params]
   (-> (Gson.)
      (.toJson (Charge/create charge-params (default-request-options)))
