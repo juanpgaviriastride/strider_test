ecabf6677d91654e61ba81facb11b6c86c0afc7a
Hernan Metaute <hernan.metaute@gmail.com>
2015-11-25 22:31:16 -0500

    clean up of the code and responses

--- pre-launch/src/pre_launch/model/stripe_charge.clj
+++ pre-launch/src/pre_launch/model/stripe_charge.clj
@@ -2,14 +2,6 @@
   (:require [pre-launch.db.core :as db]
             [clojure.data.json :as json]))
 
-(defn serialize-json [map attr-name]
-  (update-in map attr-name (json/write-str (attr-name map))))
-
-(defn prepare-create [api-response]
-  (-> api-response
-     (serialize-json :source)
-     (serialize-json :fraudDetails)
-     (serialize-json :metadata)))
-
 (defn create-charge! [stripe-api-response]
-  (db/create-charge<! stripe-api-response))
+  (let [maybe-charged (db/create-charge<! stripe-api-response)]
+    (assoc stripe-api-response :id (:generated_key maybe-charged))))
