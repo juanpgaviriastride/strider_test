7c312d6510ebe738a9b3bf941e492138426a43fc
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2015-11-25 22:31:24 -0500

    Prelaunch request invite and prelaunch user
    And handle referer tracking

---
+++ consumers/src/com/webtalk/pre_launch/request_invite.clj
@@ -0,0 +1,36 @@
+(ns com.webtalk.pre-launch.request-invite
+  (:gen-class)
+  (:require [clojurewerkz.titanium.graph    :as tgraph]
+            [clojurewerkz.titanium.vertices :as tvertex]
+            [clojurewerkz.titanium.edges    :as tedge]
+            [com.webtalk.mailer.invite      :as deliver-invite]))
+
+(defn request-hash [payload]
+  (into {} (list payload
+                 {:VertexType "prelaunchRequestedInvitation"})))
+
+(defn request-an-invite!
+  "It creates the user invitation node if needed adding links
+
+   Example: (request-an-invite! graph {\"email\" \"some@wt.com\"
+                                       \"refererID\" 234})
+   email: is the email of the invited user
+   refererID is the titan id of the user who is referring the new user and it is optional"
+
+  [graph payload]
+  (let [{email "email" referer-id "refererID"} payload
+        invitation (first (tvertex/find-by-kv graph :email email))
+        referer (first (tvertex/find-by-id (Integer. referer-id)))
+        invitation-hash (request-hash {:email email})]
+    (if (nil? invitation)
+      (let [new-invitation (tvertex/create! g invitation-hash)]
+        (tedge/upconnect! graph new-invitation "refered_by" referer)
+        {:vertex (tvertex/to-map new-invitation)
+         :status :new_record})
+      (do
+        (tedge/upconnect! graph invitation "refered_by" referer)        
+        (if (= (tvertex/get invitation :VertexType) "prelaunchRequestedInvitation")
+          {:vertex (tvertex/to-map (tvertex/merge! invitation invitation-hash))
+          :status :updated_record}
+         {:vertex (tvertex/to-map invitation)
+          :status :mixmatch_type_record})))))
---
+++ consumers/src/com/webtalk/pre_launch/user.clj
@@ -0,0 +1,30 @@
+(ns com.webtalk.pre-launch.user
+  (:gen-class)
+  (:require [clojurewerkz.titanium.graph :as tgraph]
+            [clojurewerkz.titanium.vertices :as tvertex]
+            [clojurewerkz.titanium.edges :as tedge]))
+
+(defn user-hash [payload]
+  (into {}  (list payload
+                  {:VertexType "prelaunchUser"
+                   :TeamTrainer false
+                   :Commission 0})))
+
+(defn create-user!
+  "Given a graph and user payload, creates a vertex within titandb,
+   with the provide user properties, and creating the proper network connections.
+   refererID is the titan id of the user who is referring the new user and it is optional
+  Example: (create-user! graph {\"name\" \"Sebastian\" 
+                                \"email\" \"sebastian@email.com\"
+                                \"refererID\" 234})"
+
+  [graph payload]
+  (println "create-user! graph" graph "payload" payload)
+  (let [{email "email" referer-id "refererID"} payload
+        referer (first (tvertex/find-by-id (Integer. referer-id)))
+        new-user (tvertex/create! graph (user-hash payload))]
+    (println "new-user" new-user)
+    (println "referer" referer)
+    (when referer
+      (tedge/upconnect! graph new-user "refered_by" referer))
+    (tvertex/to-map new-user)))
