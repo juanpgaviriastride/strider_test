f1db0053421899344bfc0db1b7ab8af799850892
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2015-11-30 17:02:51 -0500

    Add get-level calculations
    And invite count

--- consumers/src/com/webtalk/pre_launch/computation.clj
+++ consumers/src/com/webtalk/pre_launch/computation.clj
@@ -3,20 +3,15 @@
   (:require [clojurewerkz.titanium.graph :as tgraph]
             [clojurewerkz.titanium.vertices :as tvertex]
             [clojurewerkz.titanium.edges :as tedge]
-            ;;[ogre.core :as oq]
-            ))
+            [clojurewerkz.ogre.core :as oq]))
 
-(defn get-level [root-node edge-tag]
-  (tvertex/incoming-edges-of root-node edge-tag))
+(defn deep [q n]
+  (if (= n 1)
+    q
+    (recur (oq/<-- q ["refered_by"]) (dec n))))
 
-(defn get-level-list [sibling-list]
-  {:refered-by-me (map #(get-level % "refered_by") sibling-list)
-   :invited-by-me (map #(get-level % "invited_by") sibling-list)
-   :waiting-because-of-me (map #(get-level % "invited_waitlist_by") sibling-list)})
-
-(defn dashboard [root]
-  (let [first-lvl (get-level-list [root])
-        second-lvl (get-level-list (:refered-by-me first-lvl))
-        third-lvl (get-level-list (:refered-by-me second-lvl))
-        fourth-lvl (get-level-list (:refered-by-me third-lvl))
-        fifth-lvl (get-level-list (:refered-by-me fourth-lvl))]))
+(defn get-level [root-node lvl edge-tag]
+  (oq/query root-node
+            (deep lvl)
+            (oq/<-- [edge-tag])
+            oq/count!))
