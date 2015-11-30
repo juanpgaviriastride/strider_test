18c0a652270bbfe361b8cc98725f4c14529722ea
Hernan Metaute <hernan.metaute@gmail.com>
2015-11-29 23:09:40 -0500

    added referal code url into user referal

--- pre-launch/src/pre_launch/routes/dashboard.clj
+++ pre-launch/src/pre_launch/routes/dashboard.clj
@@ -8,8 +8,9 @@
 
 
 (defn dashboard [params session]
-  (let [user-name (get-in session [:identity :name])]
-    (layout/render "crowdfunding/dashboard.html" {:name user-name})))
+  (let [user-name (get-in session [:identity :name])
+        titan-id (get-in session [:identity :titan_id])]
+    (layout/render "crowdfunding/dashboard.html" {:name user-name :titan-id titan-id})))
 
 (defroutes dashboard-routes
   (GET "/dashboard" request (dashboard (request :params) (request :session))))
