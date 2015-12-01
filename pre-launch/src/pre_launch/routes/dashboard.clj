31f21cf0f693fc90ad8c9354e2724fc63896c9a9
Hernan Metaute <hernan.metaute@gmail.com>
2015-11-30 21:51:59 -0500

    added naive approach at all five levels

--- pre-launch/src/pre_launch/routes/dashboard.clj
+++ pre-launch/src/pre_launch/routes/dashboard.clj
@@ -18,9 +18,19 @@
                     :joined-prelaunch (controller/get-joined-prelaunch titan-id 1)
                     :sent-invites-2 (controller/get-sent-invites titan-id 2)
                     :joined-prelaunch-2 (controller/get-joined-prelaunch titan-id 2)
-                    :joined-waitlist-2 (controller/get-joined-waitlist titan-id 2)})))
+                    :joined-waitlist-2 (controller/get-joined-waitlist titan-id 2)
+                    :sent-invites-3 (controller/get-sent-invites titan-id 3)
+                    :joined-prelaunch-3 (controller/get-joined-prelaunch titan-id 3)
+                    :joined-waitlist-3 (controller/get-joined-waitlist titan-id 3)
+                    :sent-invites-4 (controller/get-sent-invites titan-id 4)
+                    :joined-prelaunch-4 (controller/get-joined-prelaunch titan-id 4)
+                    :joined-waitlist-4 (controller/get-joined-waitlist titan-id 4)
+                    :sent-invites-5 (controller/get-sent-invites titan-id 5)
+                    :joined-prelaunch-5 (controller/get-joined-prelaunch titan-id 5)
+                    :joined-waitlist-5 (controller/get-joined-waitlist titan-id 5)
+                    })))
 
-(defn sent-invites [session]
+(Defn sent-invites [session]
   (ok {:invites (controller/get-sent-invites (get-in session [:identity :titan_id]))}))
 
 (defroutes dashboard-routes
