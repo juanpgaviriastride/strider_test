8d3165d1b7f5f6ae15cba2be429300589d486710
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2015-12-02 15:56:06 -0500

    Login and Signup redirect to dashboard when logged

--- pre-launch/src/pre_launch/routes/login.clj
+++ pre-launch/src/pre_launch/routes/login.clj
@@ -6,6 +6,7 @@
             [pre-launch.model.user :as user]
             [clojure.java.io :as io]
             [pre-launch.integration.stripe :as stripe]
+            [buddy.auth :refer [authenticated?]]
             [ring.util.response :refer [response redirect]]
             ring.middleware.session))
 
@@ -39,9 +40,11 @@
   (layout/render "crowdfunding/recover-password.html"))
 
 (defroutes login-routes
-  (GET  "/signin" request (login request))
+  (GET  "/signin" request (if (authenticated? request)
+                            (redirect "/dashboard")
+                            (login request)))
   (POST "/login"  request (login! request))
   (GET "/logout"  request (logout! request))
-  (GET "/recover-password" request (recover-password request))
-  (GET  "/signup" request (signup request)))
-
+  (GET  "/signup" request (if (authenticated? request)
+                            (redirect "/dashboard")
+                            (signup request))))
