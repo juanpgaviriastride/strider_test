a82e6582c87b408826677fe9e201cd2cb4cd2239
Hernan Metaute <hernan.metaute@gmail.com>
2015-12-03 01:12:06 -0500

    added mailer with template

--- pre-launch/src/pre_launch/controllers/receipt.clj
+++ pre-launch/src/pre_launch/controllers/receipt.clj
@@ -1,5 +1,7 @@
 (ns pre-launch.controllers.receipt
   (:require [pre-launch.model.receipt :as model]
+            [selmer.parser :as parser]
+            [sendgrid-java-wrapper.core :as mailer]
             [clojure.data.json :as json]))
 
 (defn filter-data [response]
@@ -10,12 +12,19 @@
      :payment-method {:brand-name (:brand payment)
                       :last4  (:last4 payment)
                       }
-     :ammount (:amount response)
-     }
-    )
-  )
+     :ammount (:amount response)}))
 
 
 
 (defn get-payment-detail [user-id]
   (filter-data (model/receipt-data user-id)))
+
+
+(defn deliver-email [email user-id payment-data]
+  (mailer/send-email auth
+                     {:to user-email
+                      :from "team@webtalk.co"
+                      :subject "Webtalk | Payment receipt"
+                      :html (parser/render-file
+                             "emails/payment-confirmation.html"
+                             (get-payment-detail user-id))}))
