96d970458ecd4f767ee77b6a9b75e42a4f9a6f71
Hernan Metaute <hernan.metaute@gmail.com>
2015-12-03 13:03:27 -0500

    fixed email sending with receipt

--- pre-launch/src/pre_launch/controllers/receipt.clj
+++ pre-launch/src/pre_launch/controllers/receipt.clj
@@ -1,8 +1,14 @@
 (ns pre-launch.controllers.receipt
   (:require [pre-launch.model.receipt :as model]
             [selmer.parser :as parser]
+            [environ.core :refer [env]]
             [sendgrid-java-wrapper.core :as mailer]
             [clojure.data.json :as json]))
+
+
+(def auth {:api_user (or (env :sengrid-username) "sarcilav")
+           :api_key  (or (env :sengrid-key) "P@wU8Z#wGHAZ^n")})
+
 
 (defn filter-data [response]
   (let [payment (json/read-str (:source response) :key-fn keyword)]
@@ -14,17 +20,16 @@
                       }
      :ammount (:amount response)}))
 
-
-
 (defn get-payment-detail [user-id]
   (filter-data (model/receipt-data user-id)))
 
 
-(defn deliver-email [email user-id payment-data]
-  (mailer/send-email auth
-                     {:to user-email
+(defn deliver-email [email user-id]
+  (let [payment-data (get-payment-detail user-id)]
+    (mailer/send-email auth
+                     {:to email
                       :from "team@webtalk.co"
                       :subject "Webtalk | Payment receipt"
                       :html (parser/render-file
                              "emails/payment-confirmation.html"
-                             (get-payment-detail user-id))}))
+                             (get-payment-detail user-id))})))
