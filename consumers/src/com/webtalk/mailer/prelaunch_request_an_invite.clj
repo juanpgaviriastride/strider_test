84d5542ba3a281ddb4ea2325d3d7a19bf38c4323
David Alzate <david@nullindustries.co>
2015-12-13 15:56:40 -0500

    fixing error on consumer emails

--- consumers/src/com/webtalk/mailer/prelaunch_request_an_invite.clj
+++ consumers/src/com/webtalk/mailer/prelaunch_request_an_invite.clj
@@ -10,7 +10,7 @@
                      {:to to
                       :from "team@webtalk.co"
                       :subject "Waitlist Confirmation"
-                      :html (slurp (io/resource "templates/prelaunch_request_an_invite.html")
-                        {:to to
-                              :name (:name sender)
+                      :html (template/render-resource
+                             "templates/prelaunch_request_an_invite.html.mustache"
+                             {:to to
                               :join_url (str config/base-url)})}))
