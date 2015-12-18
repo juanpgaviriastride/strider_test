7fcea6aadc724d4bc55af62ede41e86141b300b2
Hernan Metaute <hernan.metaute@gmail.com>
2015-12-18 15:42:59 -0500

    the flash messages now sending in session

--- pre-launch/src/pre_launch/routes/home.clj
+++ pre-launch/src/pre_launch/routes/home.clj
@@ -11,12 +11,13 @@
             [taoensso.timbre :refer [spy]]
             [clojure.java.io :as io]))
 
-(defn home-page [success]
+(defn home-page [flash-message session]
   (let [params-hash (scontroller/get-overal-app-stats)
-        final-params (assoc params-hash :success success)]
+        final-params (merge params-hash flash-message)]
     (filter/add-filter! :money-format (fn [amount] (format (Integer. "$%,8d%n"))))
-    (layout/render
-     "home.html" final-params)))
+    (->  (layout/render
+         "home.html" final-params)
+        (update-in [:session] dissoc :flash-message))))
 
 (defn refered-home-page [{params :params session :session}]
   (let [referer-id (:titan_id params)]
@@ -25,23 +26,28 @@
      (assoc :session (assoc session :refererID referer-id)))))
 
 
-(defn decide-home [request success]
+(defn decide-home [request]
   (let [requesting-url (get-in request [:headers "host"])
-        wt-url (config/webtalk-url)]
+        wt-url (config/webtalk-url)
+        flash-message (get-in request [:session :flash-message] {})]
     (spy requesting-url)
     (spy wt-url)
+    (spy flash-message)
     (if (= wt-url requesting-url) 
-      (layout/render "webtalk/home.html" {:success  success})
-      (home-page success))))
+      (layout/render "webtalk/home.html" flash-message)
+      (home-page flash-message (:session request)))))
 
 (defn join-wait-list [{session :session params :params :as req}]
   (controller/join-wait-list (:email params) (:refererID session))
-  (decide-home req true))
+  (-> (redirect "/")
+     (assoc :session
+            (assoc session :flash-message {:success true
+                                           :message "You successfully joined the waiting list. We'll contact you!"}))))
 
 
 
 (defroutes home-routes
-  (GET "/" request (decide-home request false))
+  (GET "/" request (decide-home request))
   (GET "/invite/:titan_id" request (refered-home-page request))
   (POST "/request-invitation" request (join-wait-list request)))
 
