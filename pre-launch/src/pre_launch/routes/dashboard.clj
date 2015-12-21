d681477055370f61ab9150e43f34602a8e2eee69
David Alzate <david@nullindustries.co>
2015-12-21 15:07:50 -0500

    adding Rerered Name to dashboard.html

--- pre-launch/src/pre_launch/routes/dashboard.clj
+++ pre-launch/src/pre_launch/routes/dashboard.clj
@@ -3,23 +3,31 @@
             [compojure.core :refer [defroutes GET POST]]
             [pre-launch.config-mailer :as config-mailer]
             [ring.util.http-response :refer [ok]]
+            [pre-launch.model.user :as user-model]
             [pre-launch.controllers.dashboard :as controller]
             [ring.util.response :refer [response]]
             [selmer.filters :as filter]
             [clojure.java.io :as io]
             [taoensso.timbre :refer [spy]]))
 
-(defn prepare-response [titan-id user-name]
+(defn prepare-response [titan-id user-name referer-name]
   {:name user-name
    :titan-id titan-id
    :network-table (controller/get-referral-network titan-id)
+   :referer-name referer-name
    :join_url (str (config-mailer/root-url) "/invite/" titan-id)})
 
 
 (defn dashboard [params session]
   (let [user-name (get-in session [:identity :name])
         titan-id (get-in session [:identity :titan_id])
-        template-response (prepare-response titan-id user-name)]
+        referer-data (if-let [referer-id   (:refererID session)]
+                      (do
+                        (spy(referer-id)) 
+                        (user-model/email-by-titan referer-id))
+                      {} 
+                    )
+        template-response (prepare-response titan-id user-name (:name referer-data))]
     (spy template-response)
     (filter/add-filter! :monetizise (fn [amount] (format "$%,8d%n"(* (Integer. amount) 10))))
     (layout/render "crowdfunding/dashboard.html" template-response)))
