72c5cd02d057c6a9c45e09d8677a3f57e3eff596
David Alzate <david@nullindustries.co>
2015-12-28 16:15:33 -0500

    add info to debug error while sending invitations

--- pre-launch/src/pre_launch/routes/invitation.clj
+++ pre-launch/src/pre_launch/routes/invitation.clj
@@ -5,13 +5,13 @@
             [pre-launch.controllers.invitation :as controller]
             [ring.util.response :refer [redirect response]]
             [clojure.java.io :as io]
-            [taoensso.timbre :refer [debug]]))
+            [taoensso.timbre :refer [debug info]]))
 
 (defn send-invitation! [request]
-  (debug "save invitation the invitations are:" (get-in request [:params :emails]))
+  (info "save invitation the invitations are:" (get-in request [:params :emails]))
   (let [referer-id (get-in request [:session :identity :titan_id])
         emails (get-in request [:params :emails])]
-    (debug "save invitation the referer ID is:" referer-id)
+    (info "save invitation the referer ID is:" referer-id)
     (controller/send-invitation emails referer-id)
     (ok)))
 
