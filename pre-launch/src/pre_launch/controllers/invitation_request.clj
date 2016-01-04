2edd0a1126a5beb77184ec87d62b91105447180f
David Alzate <david@nullindustries.co>
2016-01-04 14:42:24 -0500

    send email of referral when joining waitlist

--- pre-launch/src/pre_launch/controllers/invitation_request.clj
+++ pre-launch/src/pre_launch/controllers/invitation_request.clj
@@ -1,6 +1,8 @@
 (ns pre-launch.controllers.invitation-request
   (:require [clj-wt.queue :as queue]
             [pre-launch.model.invitation-request :as model]
+            [taoensso.timbre :refer [spy]]
+            [pre-launch.controllers.referal-notification :as referal-controller]
             [crypto.random :refer [url-part]]))
 
 
@@ -8,4 +10,6 @@
   (let [callback-queue-name (str "crowdfunding.webtalk." (url-part 15))
         response (future (model/save-invitation-request email maybe-referer-id))]
     (queue/publish-with-qname "com.webtalk.pre-launch.request-an-invite" callback-queue-name {:email email
-                                                                                              :refererID maybe-referer-id})))
+                                                                                              :refererID maybe-referer-id})
+    (when maybe-referer-id
+ 		(referal-controller/notify-referer maybe-referer-id email))))
