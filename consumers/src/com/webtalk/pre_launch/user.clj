6509f092700db6541811f0dc5437d38aab8c9ec6
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2016-01-06 18:35:53 -0500

    Add a relink function and avoid inconsistent nodes

--- consumers/src/com/webtalk/pre_launch/user.clj
+++ consumers/src/com/webtalk/pre_launch/user.clj
@@ -26,10 +26,29 @@
           referer (when referer-id
                     (tvertex/find-by-id g (Integer. referer-id)))
           [user status] (if-let [maybe-user (first (tvertex/find-by-kv g :email email))]
-                          [maybe-user :old]
+                          (if (= "prelaunchUser" (tvertex/get maybe-user :VertexType))
+                            [maybe-user :old]
+                            [(tvertex/merge! maybe-user (user-hash payload)) :update])                          
                           [(tvertex/create! g (user-hash payload)) :new])]
-      (when (and (= status :new) referer)
+      (when (and (not= status :old) referer)
         (tedge/upconnect! g user "refered_by" referer {:time (System/currentTimeMillis)}))
       (spy user)
       (spy referer)
+      (tvertex/to-map user))))
+
+(defn change-user-and-link [graph email ]
+  (tgraph/with-transaction [g graph]
+    (let [[user status] (when-let [maybe-user (first (tvertex/find-by-kv g :email email))]
+                          (if (= "prelaunchUser" (tvertex/get maybe-user :VertexType))
+                            [maybe-user :old]
+                            [(tvertex/merge! maybe-user (user-hash {})) :update]))
+          referer-edge (first (tvertex/outgoing-edges-of user "invited_by"))
+          referer (when referer-edge
+                    (tedge/head-vertex referer-edge))]
+      
+      (when (and (not= status :old) referer)
+        (spy "updating")
+        (spy (tvertex/to-map referer))
+        (tedge/upconnect! g user "refered_by" referer {:time (System/currentTimeMillis)}))
+      (spy (tvertex/to-map user))
       (tvertex/to-map user))))
